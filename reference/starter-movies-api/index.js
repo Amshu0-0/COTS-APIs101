304905e0572e1a1c199c2721ead0e045b5f9ec2c
Amshu0-0 <amshuw05@gmail.com>
2025-05-13 02:10:09 -0500

    completed movies api

--- reference/starter-movies-api/index.js
+++ reference/starter-movies-api/index.js
@@ -7,6 +7,18 @@ app.get("/about", (req, res) => {
 	res.send("This is an API service for CRUD actions on movies resources.");
 });
 
+app.get("/about/:name",(req, res) => {
+	const name = req.params.name;
+	res.send("This is an API service for CRUD actions on a movies resource...for you," + name);
+})
+
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+const movieRouter = require('./routes/movies_routes');
+app.use('/movies', movieRouter);
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}.`);
 });
